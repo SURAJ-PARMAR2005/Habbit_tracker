36668bc8358a3f9846d17050c014354384e42af6
SURAJ-PARMAR2005 <golurajput87080@gmail.com>
2026-03-06 22:07:03 +0530

    Refactor updateDailyData function to enhance progress tracking and user experience

--- Backend/src/controllers/daily.controller.js
+++ Backend/src/controllers/daily.controller.js
@@ -1,5 +1,5 @@
 const dailyModel  = require("../models/dailyprogress.model")
-
+const userModel = require("../models/user.model")
 const getTodayDate = () => {
      return new Date().toISOString().split("T")[0];
 }
@@ -37,24 +37,98 @@ async function getDailyData(req,res) {
 }
 
 async function updateDailyData(req,res){
-    const userId = req.userId;
-    const {data} = req.body;
     try {
-        const user = await dailyModel.findOneAndUpdate({_id: userId},
-            {quest:data.quest,
-            extra:data.extra,
-            taskCompleted:data.taskCompleted,
-            totalXpEarnedToday:data.totalXpEarnedToday,
-            }
-        )
+     const userId = req.userId;
+    const {type,name} = req.body;
+    const today = getTodayDate();
 
-        if(user){
-          return res.status(201).json({
-                message: "Data Updated successfully",
-                user,
+    const progress = await dailyModel.findOne({
+        userId,
+        date: today,
+    })
+
+    if(!progress){
+        progress = await dailyModel.create({
+            userId,
+            date: today,
+        })
+    }
+
+    const user = await userModel.findById(userId);
+
+    if(!user){
+        return res.status(401).json({
+            message: "User Not found",
+        })
+    }
+
+    let xpReward = 0;
+    let StatUpdate = {};
+
+    if(type == "quest"){
+        if(progress.quest[name]){
+            return res.json({
+                message : "quest Already completed",
+                progress,
             })
         }
-        
+
+        progress.quest[name] = true;
+
+        progress.taskCompleted += 1;
+        xpReward = 100;
+        if(name == "study"){
+        StatUpdate.intelligence = 1;
+        xpReward = 100;
+        }
+         if(name == "gym"){
+        StatUpdate.strength = 1;
+        xpReward = 100;
+        } 
+        if(name == "meditation"){
+        StatUpdate.wisdom = 1;
+        xpReward = 100;
+        }
+    }
+
+    if(type == "extra"){
+        progress.extra[name] += 1;
+        xpReward = 50;
+    }
+    
+
+    user.xp += xpReward;
+    progress.totalXpEarnedToday += xpReward;
+
+    if(StatUpdate.strength) {
+        user.stats[strength] += StatUpdate.strength;
+    }
+    if(StatUpdate.intelligence){
+        user.stats[intelligence] += StatUpdate.intelligence;
+    }
+    if(StatUpdate.wisdom){
+        user.stats[wisdom] += StatUpdate.wisdom;
+    }
+    
+     const LEVEL_THRESHOLD = 1000;
+
+    if (user.xp >= LEVEL_THRESHOLD) {
+      user.level += 1;
+      user.xp = user.xp % LEVEL_THRESHOLD;
+      LEVEL_THRESHOLD += 500;
+    }
+
+    // 8️⃣ SAVE
+    await progress.save();
+    await user.save();
+
+    // 9️⃣ RESPONSE
+    return res.status(200).json({
+      message: "Progress updated",
+      progress,
+      user
+    });
+           
     } catch (error) {
         console.log(error);
         return res.status(401).json({
