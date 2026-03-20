19e610b7350eaa523e2116ba3a882159d747dce5
SURAJ-PARMAR2005 <golurajput87080@gmail.com>
2026-03-20 23:42:58 +0530

    Refactor server and app setup: update port configuration and enhance CORS comment for clarity

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,11 @@
 const app = require("./src/app");
 const connectDB = require("./src/db/db");
+require("dotenv").config();
 
 connectDB(); 
 
+const Port = process.env.PORT || 3000;
+
 app.listen(3000,() => {
-    console.log("server up baby at port 3000");
+    console.log(`server up baby at port ${Port}`);
 })
